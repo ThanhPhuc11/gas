64c3e848c01d060752f72a9c52a7d4b4e5afbb01
phucmt <mbat243@gmail.com>
2021-01-03 12:31:22 +0700

    fix viewmodel_factory

--- app/src/main/java/vn/gas/thq/base/ViewModelFactory.kt
+++ app/src/main/java/vn/gas/thq/base/ViewModelFactory.kt
@@ -2,16 +2,22 @@ package vn.gas.thq.base
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import vn.gas.thq.network.ApiHelper
 import vn.gas.thq.network.ApiService
 import vn.gas.thq.ui.login.LoginRepository
 import vn.gas.thq.ui.login.LoginViewModel
 
 class ViewModelFactory(private val apiService: ApiService) : ViewModelProvider.Factory {
+    var creators = mutableMapOf<Class<out ViewModel>, ViewModel>()
+
+    init {
+        creators[LoginViewModel::class.java] = LoginViewModel(LoginRepository(apiService))
+    }
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
-            return LoginViewModel(LoginRepository(apiService)) as T
+        for (a in creators.entries) {
+            if (modelClass.isAssignableFrom(a.key)) {
+                return a.value as T
+            }
         }
         throw IllegalArgumentException("Unknown class name")
     }
