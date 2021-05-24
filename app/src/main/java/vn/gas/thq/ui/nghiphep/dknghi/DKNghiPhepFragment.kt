cd040a58816c76feb66a5601c1edb0a57a8161b0
neji <gmail>
2021-05-24 16:44:33 +0700

    ghep api nghi phep

--- app/src/main/java/vn/gas/thq/ui/nghiphep/dknghi/DKNghiPhepFragment.kt
+++ app/src/main/java/vn/gas/thq/ui/nghiphep/dknghi/DKNghiPhepFragment.kt
@@ -1,14 +1,24 @@
 package vn.gas.thq.ui.nghiphep.dknghi
 
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.fragment_dang_ky_nghi.*
+import kotlinx.android.synthetic.main.fragment_dang_ky_nghi.edtEndDate
+import kotlinx.android.synthetic.main.fragment_dang_ky_nghi.edtStartDate
+import kotlinx.android.synthetic.main.fragment_lich_su_nghi.*
 import vn.gas.thq.base.BaseFragment
 import vn.gas.thq.base.ViewModelFactory
 import vn.gas.thq.network.ApiService
 import vn.gas.thq.network.RetrofitBuilder
+import vn.gas.thq.util.AppDateUtils
+import vn.gas.thq.util.CommonUtils
 import vn.hongha.ga.R
 
 class DKNghiPhepFragment : BaseFragment() {
     lateinit var viewModel: DKNghiPhepViewModel
+
     companion object {
         @JvmStatic
         fun newInstance() = DKNghiPhepFragment()
@@ -39,10 +49,68 @@ class DKNghiPhepFragment : BaseFragment() {
     }
 
     override fun initData() {
+        edtStartDate.setText(AppDateUtils.getCurrentDate())
+        edtEndDate.setText(AppDateUtils.getCurrentDate())
+        edtStartDate.setOnClickListener {
+            CommonUtils.showCalendarDialog(
+                context,
+                edtStartDate.text.toString()
+            ) { strDate -> edtStartDate.setText(strDate) }
+        }
 
+        edtEndDate.setOnClickListener {
+            CommonUtils.showCalendarDialog(
+                context,
+                edtEndDate.text.toString()
+            ) { strDate -> edtEndDate.setText(strDate) }
+        }
+        btnRegister.setOnClickListener(this::registerVacation)
     }
 
     override fun initObserver() {
+        viewModel.callbackStart.observe(viewLifecycleOwner, {
+            showLoading()
+        })
+
+        viewModel.callbackSuccess.observe(viewLifecycleOwner, {
+            hideLoading()
+        })
+
+        viewModel.callbackFail.observe(viewLifecycleOwner, {
+            hideLoading()
+        })
+
+        viewModel.showMessCallback.observe(viewLifecycleOwner, {
+            Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+        })
+
+        viewModel.callbackRegisterSuccess.observe(viewLifecycleOwner, {
+            CommonUtils.showDiglog1Button(
+                activity,
+                "Thông báo",
+                "Bạn đã đăng ký nghỉ phép thành công"
+            ) {
+
+            }
+        })
+    }
+
+    private fun registerVacation(view: View) {
+        val fromDate =
+            AppDateUtils.changeDateFormat(
+                AppDateUtils.FORMAT_2,
+                AppDateUtils.FORMAT_5,
+                edtStartDate.text.toString()
+            )
+        val endDate =
+            AppDateUtils.changeDateFormat(
+                AppDateUtils.FORMAT_2,
+                AppDateUtils.FORMAT_5,
+                edtEndDate.text.toString()
+            )
+        if (!TextUtils.isEmpty(edtLyDo.text)) {
+            viewModel.registerVacation(1, fromDate, endDate, edtLyDo.text.toString())
+        }
 
     }
 }
--- app/src/main/java/vn/gas/thq/ui/nghiphep/dknghi/DKNghiPhepViewModel.kt
+++ app/src/main/java/vn/gas/thq/ui/nghiphep/dknghi/DKNghiPhepViewModel.kt
@@ -1,7 +1,33 @@
 package vn.gas.thq.ui.nghiphep.dknghi
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 import vn.gas.thq.base.BaseViewModel
 
-class DKNghiPhepViewModel(private val dkNghiPhepRepository: DKNghiPhepRepository) :
+class DKNghiPhepViewModel(private val repository: DKNghiPhepRepository) :
     BaseViewModel() {
+    val callbackRegisterSuccess = MutableLiveData<Unit>()
+    fun registerVacation(staffId: Int, fromDate: String, toDate: String, reason: String) {
+        viewModelScope.launch {
+            repository.registerVacation(staffId, fromDate, toDate, reason)
+                .onStart {
+                    callbackStart.value = Unit
+                }
+                .onCompletion {
+
+                }
+                .catch {
+                    handleError(it)
+                }
+                .collect {
+                    callbackSuccess.value = Unit
+                    callbackRegisterSuccess.value = Unit
+                }
+        }
+    }
 }
